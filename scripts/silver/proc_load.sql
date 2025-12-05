f187047fb5a70f9b58513bf7c00a54558a978d83
tusharpatgar <117505866+tusharpatgar@users.noreply.github.com>
2025-12-05 19:41:38 +0530

    Update proc_load.sql

--- scripts/silver/proc_load.sql
+++ scripts/silver/proc_load.sql
@@ -28,4 +28,31 @@ from [bronze].[crm_cust_info]
 WHERE cst_id is not null
 )t where flag_LAST=1
 
- 
+ INSERT INTO silver.crm_prd_info (
+			prd_id,
+			cat_id,
+			prd_key,
+			prd_nm,
+			prd_cost,
+			prd_line,
+			prd_start_dt,
+			prd_end_dt
+		)
+select 
+prd_id,
+REPLACE(SUBSTRING(prd_key,1,5),'-','_') as cat_id,
+SUBSTRING(prd_key,7,LEN(prd_key)) as prd_key,
+prd_nm,
+isnull(prd_cost,0) as prd_cost,
+CASE WHEN prd_line='M' THEN 'Mountain'
+	 WHEN prd_line='R' THEN 'Roads'
+	 WHEN prd_line='S' THEN 'Other Roads'
+	 WHEN prd_line='T' THEN 'Touring'
+	 ELSE 'N/A'
+END as prd_line,
+CAST(CAST(prd_start_dt as varchar) as DATE) as prd_start_dt,
+CAST(
+		LEAD(prd_start_dt) OVER (PARTITION BY prd_key ORDER BY prd_start_dt) - 1 
+		AS DATE
+) AS prd_end_dt
+ from [bronze].[crm_prd_info]
